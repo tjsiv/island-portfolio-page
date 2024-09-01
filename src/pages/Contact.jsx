eee36a78fef2ae6e4b3d7970bd7db0ac141c136f
tjsiv <tjsivelsjr@gmail.com>
2024-09-01 12:28:33 -0700

    added contact models alerts and stylings

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,29 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, Suspense} from 'react'
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber'
+
+import Loader from '../components/Loader'
+import Fox from '../models/Fox'
+import useAlert from '../hooks/useAlert'
+import Alert from '../components/Alert'
 
 const Contact = () => {
   const formRef = useRef(null)
   const [form, setForm] = useState({name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false)
+  const [currentAnimation, setcurrentAnimation] = useState('idle')
+  const {alert, showAlert, hideAlert} = useAlert()
 
   const handleChange = (e) =>{
     setForm({...form, [e.target.name]: e.target.value})
   }
-  const handleFocus = () =>{}
-  const handleBlur = () =>{}
+  const handleFocus = () =>setcurrentAnimation('walk')
+  const handleBlur = () =>setcurrentAnimation('idle')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     setIsLoading(true)
+    setcurrentAnimation('hit')
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -29,16 +38,36 @@ const Contact = () => {
       }, import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(() => {
       setIsLoading(false)
-      setForm({name: '', email: '', message: ''})
+      showAlert({
+        show: true,
+        message: 'Your message has been sent successfully!',
+        type:'success'
+        
+      })
+      
+      setTimeout(() => {
+        hideAlert()
+        setcurrentAnimation('idle')
+        setForm({name: '', email: '', message: ''})
+      }, 3000)
 
     }).catch((error) =>{
+      setcurrentAnimation('idle')
       setIsLoading(false)
+      showAlert({
+        show: true,
+        message: 'Your message has not been succesfully sent!',
+        type:'danger'
+        
+      })
       console.error('Failed to send email: ', error);
       
     })
   }
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert {...alert} />}
+
       <div className='flex-1 min-w-[50%] flex flex-col '>
         <h1 className='head-text'>Get in Touch</h1>
 
@@ -58,7 +87,7 @@ const Contact = () => {
             />
           </label>
           <label className='text-black-500 font-semibold'>
-            Name
+            Email
             <input 
               className='input'
               name='email'
@@ -72,7 +101,7 @@ const Contact = () => {
             />
           </label>
           <label className='text-black-500 font-semibold'>
-            Name
+            Message
             <textarea 
               className='textarea'
               name='message'
@@ -95,6 +124,28 @@ const Contact = () => {
             
           </button>
         </form>
+      </div>
+      <div className='lgw-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+      <Canvas
+        camera={{
+          position: [0, 0, 5],
+          fov: 75,
+          near: 0.1,
+          far: 1000
+        }}
+      >
+        <ambientLight intensity={0.5} />
+        <directionalLight intensity={2.5} position={[0,0,1]} />
+        <Suspense fallback={<Loader/>}>
+          <Fox
+          currentAnimation={currentAnimation}
+            position = {[0.5, 0.35, 0]}
+            rotattion={[12, -0.6, 0]}
+            scale = {[0.5, 0.5, 0.5]}
+            
+          />
+        </Suspense>
+      </Canvas>
       </div>
     </section>
   )
